fc91475bc5d04b803ab05001c6c3532635abbaab
biobob <robert.komorovsky@gmail.com>
2020-03-03 18:05:37 +0100

    Simplification by removing unnecessary annotations

--- quarkus/src/main/java/sk/p8z/quarkus/resources/MoviesResource.java
+++ quarkus/src/main/java/sk/p8z/quarkus/resources/MoviesResource.java
@@ -9,9 +9,6 @@ import sk.p8z.quarkus.apis.MovieApi;
 import sk.p8z.quarkus.entities.Movie;
 import sk.p8z.quarkus.repositories.MoviesRepository;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @RestController
@@ -30,18 +27,18 @@ public class MoviesResource implements MovieApi {
     }
 
     @Override
-    public ResponseEntity<Movie> moviesMovieIdGet(@PathVariable Integer id) {
+    public ResponseEntity<Movie> moviesMovieIdGet(Integer id) {
         return ResponseEntity.ok(moviesRepository.findById(id.intValue()));
     }
 
     @Override
-    public ResponseEntity<Movie> moviesMoviePost(@Valid @RequestBody Movie body) {
+    public ResponseEntity<Movie> moviesMoviePost(Movie body) {
         body.setId(null);
         return ResponseEntity.ok(moviesRepository.save(body));
     }
 
     @Override
-    public ResponseEntity<List<Movie>> moviesTitleGet(@Valid @NotEmpty @NotBlank @PathVariable String title) {
+    public ResponseEntity<List<Movie>> moviesTitleGet(String title) {
         return ResponseEntity.ok(moviesRepository.findMoviesByLowercaseTitleContaining(title.toLowerCase()));
     }
 }
